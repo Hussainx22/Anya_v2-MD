6285a5e899511e67ef34fa8646741bd800b71e11
Hussainx22 <botx2025@gmail.com>
2024-09-23 22:15:56 +0600

    Update config.js

--- config.js
+++ config.js
@@ -9,28 +9,28 @@ module.exports = {
      * bot details and parameters
      */
     botname: process.env.BotName || "Queen Anya Bot",
-    footer: process.env.Footer || "© Powered By Queen Anya Bot",
-    prefa: process.env.Prefix || "-",
+    footer: process.env.Footer || "© Powered By Hussain Bot",
+    prefa: process.env.Prefix || "!",
     themeemoji: process.env.ThemeEmoji || "🎐",
 
     /**
      * owner details and parameters
      */
-    ownername: process.env.Owner_Name || "Pika~Kun",
-    ownernumber: process.env.Owner_Number || "916900904828",
-    instagramId: process.env.Insta || "3.69_pika",
+    ownername: process.env.Owner_Name || "Hussain Ahmed",
+    ownernumber: process.env.Owner_Number || "01965142856",
+    instagramId: process.env.Insta || "not use",
 
     /**
      * other details and parameters
      */
-    author: process.env.Author || "@PikaBotz",
+    author: process.env.Author || "@Hussain2370",
     packname: process.env.PackName || "Queen Anya v2 MD",
     socialLink: process.env.Web || "https://github.com/PikaBotz",
     groupLink: process.env.GcLink || "https://chat.whatsapp.com/E490r0wSpSr89XkCWeGtnX",
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: Number(process.env.Cooldown_Timer_Seconds) || 5, // default cooldown time per command in seconds
     mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
-    sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID",
+    sessionId: process.env.SESSION_ID || "dTZ2emc_Queen-Anya_xbWw=",
 
     /**
      * thumbnails and medias
